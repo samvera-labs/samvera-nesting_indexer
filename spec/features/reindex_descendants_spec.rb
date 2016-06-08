7a06b6fb03111f9c424980af6beb5f05fba0dba5
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2016-06-08 08:31:09 -0400

    Adding an additional scenario regarding graphs

--- spec/features/reindex_descendants_spec.rb
+++ spec/features/reindex_descendants_spec.rb
@@ -190,6 +190,7 @@ module Curate
     context "non-Cycle graphs" do
       [
         {
+          name: 'A semi-complicated graph with diamonds and triangle relationships',
           starting_graph: {
             parents: { a: [], b: ['a'], c: ['a', 'b'], d: ['c', 'e'], e: ['b'] },
             ancestors: { a: [], b: ['a'], c: ['a/b', 'a'], d: ['a', 'a/b', 'a/b/c', 'a/b/e', 'a/c'], e: ['a', 'a/b'] },
@@ -202,20 +203,46 @@ module Curate
             pathnames: { a: ['a'], b: ['a/b'], c: ['a/c'], d: ['a/c/d', 'a/b/e/d'], e: ['a/b/e'] }
           }
         }, {
+          name: 'Two child with same parents and one drops one of the parents',
           starting_graph: {
             parents: { a: [], b: [], c: ['a', 'b'], d: ['a', 'b'] },
             ancestors: { a: [], b: [], c: ['a', 'b'], d: ['a', 'b'] },
-            pathnames: { a: [], b: [], c: ['a/c', 'b/c'], d: ['a/d', 'b/d'] },
+            pathnames: { a: ['a'], b: ['b'], c: ['a/c', 'b/c'], d: ['a/d', 'b/d'] },
           },
           updated_attributes: { pid: :c, parents: ['a'], pathnames: ['a/c'], ancestors: ['a'] },
           ending_graph: {
             parents: { a: [], b: [], c: ['a'], d: ['a', 'b'] },
             ancestors: { a: [], b: [], c: ['a'], d: ['a', 'b'] },
-            pathnames: { a: [], b: [], c: ['a/c'], d: ['a/d', 'b/d'] }
+            pathnames: { a: ['a'], b: ['b'], c: ['a/c'], d: ['a/d', 'b/d'] }
+          }
+        }, {
+          name: 'Switching top-level parents in a nested graph',
+          starting_graph: {
+            parents: { a: [], b: ['a'], c: ['a', 'b'], d: ['b', 'c'], e: ['b', 'c'], f: ['e'], g: [] },
+            ancestors: {
+              a: [], b: ['a'], c: ['a', 'a/b'], d: ['a', 'a/b', 'a/b/c', 'a/c'], e: ['a', 'a/b', 'a/b/c', 'a/c'],
+              f: ['a', 'a/b', 'a/b/e', 'a/b/c', 'a/b/c/e', 'a/c', 'a/c/e'], g: []
+            },
+            pathnames: {
+              a: ['a'], b: ['a/b'], c: ['a/c', 'a/b/c'], d: ['a/b/d', 'a/b/c/d', 'a/c/d'], e: ['a/b/e', 'a/b/c/e', 'a/c/e'],
+              f: ['a/b/e/f', 'a/b/c/e/f', 'a/c/e/f'], g: ['g']
+            }
+          },
+          updated_attributes: { pid: :b, parents: ['g'], pathnames: ['g/b'], ancestors: ['g'] },
+          ending_graph: {
+            parents: { a: [], b: ['g'], c: ['a', 'b'], d: ['b', 'c'], e: ['b', 'c'], f: ['e'], g: [] },
+            ancestors: {
+              a: [], b: ['g'], c: ['a', 'g', 'g/b'], d: ['g', 'g/b', 'g/b/c', 'a', 'a/c'], e: ['g', 'g/b', 'g/b/c', 'a', 'a/c'],
+              f: ['a', 'a/c', 'a/c/e', 'g', 'g/b', 'g/b/c', 'g/b/c/e', 'g/b/e'], g: []
+            },
+            pathnames: {
+              a: ['a'], b: ['g/b'], c: ['a/c', 'g/b/c'], d: ['g/b/d', 'g/b/c/d', 'a/c/d'], e: ['g/b/e', 'g/b/c/e', 'a/c/e'],
+              f: ['g/b/e/f', 'g/b/c/e/f', 'a/c/e/f'], g: ['g']
+            },
           }
         }
       ].each_with_index do |the_scenario, index|
-        context "Scenario #{index}" do
+        context "#{the_scenario.fetch(:name)} (Scenario #{index})" do
           let(:starting_graph) { the_scenario.fetch(:starting_graph) }
           let(:updated_attributes) { the_scenario.fetch(:updated_attributes) }
           let(:ending_graph) { the_scenario.fetch(:ending_graph) }

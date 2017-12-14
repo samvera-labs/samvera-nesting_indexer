6f03f67e713ef19eabc2475907dc0e92aad5f704
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2017-12-14 10:31:20 -0500

    Refactoring to prepare for examples of nesting
    * Removing chattery logs
    * Pull useful methods into a spec support module

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,3 +3,7 @@ $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'coverage_helper'
 require 'samvera/nesting_indexer'
 require 'rspec/its'
+
+Samvera::NestingIndexer.configure do |config|
+  config.logger = Logger.new('/dev/null') unless ENV['VERBOSE'] # Remove all the chatter!
+end

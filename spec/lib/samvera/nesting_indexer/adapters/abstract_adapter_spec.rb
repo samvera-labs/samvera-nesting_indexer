2802d67429ab9344e172224fb62688a669180f17
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2018-01-11 17:55:57 -0500

    Expanding spec description to provide guidance
    From PR Conversation samvera-labs/samvera-nesting_indexer#6
    
    @elrayle
    
    > Would it be better to write this as a pending test? That way it would
    > show up in the rspec log as pending giving a reminder to come back to
    > it.
    
    @jeremyf
    
    > The methods are part of an abstract adapter; The abstract adapter
    > methods define method signature but not implementation. The class that
    > extends this adapter would then need to implement.

--- spec/lib/samvera/nesting_indexer/adapters/abstract_adapter_spec.rb
+++ spec/lib/samvera/nesting_indexer/adapters/abstract_adapter_spec.rb
@@ -9,48 +9,49 @@ module Samvera
         it_behaves_like 'a Samvera::NestingIndexer::Adapter'
         describe '.find_preservation_document_by(id:)' do
           subject { described_class.find_preservation_document_by(id: 1) }
-          it 'requires implementation (see documentation)' do
+          it 'is defined but requires implementation in classes that extend this class (see documentation)' do
             expect { subject }.to raise_error(NotImplementedError)
           end
         end
 
         describe '.find_index_document_by' do
           subject { described_class.find_index_document_by(id: 1) }
-          it 'requires implementation (see documentation)' do
+          it 'is defined but requires implementation in classes that extend this class (see documentation)' do
             expect { subject }.to raise_error(NotImplementedError)
           end
         end
 
         describe '.each_perservation_document_id_and_parent_ids' do
           subject { described_class.each_perservation_document_id_and_parent_ids }
-          it 'requires implementation (see documentation)' do
+          it 'is defined but requires implementation in classes that extend this class (see documentation)' do
             expect { subject }.to raise_error(NotImplementedError)
           end
         end
 
         describe '.find_preservation_parent_ids_for(id:)' do
           subject { described_class.find_preservation_parent_ids_for(id: 1) }
-          it 'requires implementation (see documentation)' do
+          it 'is defined but requires implementation in classes that extend this class (see documentation)' do
             expect { subject }.to raise_error(NotImplementedError)
           end
         end
 
         describe '.each_child_document_of' do
           subject { described_class.each_child_document_of(document: double) }
-          it 'requires implementation (see documentation)' do
+          it 'is defined but requires implementation in classes that extend this class (see documentation)' do
             expect { subject }.to raise_error(NotImplementedError)
           end
         end
 
         describe '.write_document_attributes_to_index_layer' do
           subject { described_class.write_document_attributes_to_index_layer(id: 1, parent_ids: 2, ancestors: 3, pathnames: 4, deepest_nested_depth: 5) }
-          it 'requires implementation (see documentation)' do
+          it 'is defined but requires implementation in classes that extend this class (see documentation)' do
             expect { subject }.to raise_error(NotImplementedError)
           end
         end
+
         describe '.write_nesting_document_to_index_layer' do
           subject { described_class.write_nesting_document_to_index_layer(nesting_document: 1) }
-          it 'requires implementation (see documentation)' do
+          it 'is defined but requires implementation in classes that extend this class (see documentation)' do
             expect { subject }.to raise_error(NotImplementedError)
           end
         end
